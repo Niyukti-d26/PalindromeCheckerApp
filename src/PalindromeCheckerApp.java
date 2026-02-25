62a0fb4fd600c51f20f931e2e1beb355d4e8903e
Niyukti-d26 <npjj26@gmail.com>
2026-02-25 11:22:27 +0530

    UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        scanner.close();
+    }
+
+    // Recursive method
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return check(s, start + 1, end - 1);
+    }
 }
