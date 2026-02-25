99edc7277e76d15dd7b4055dfc848260c0046ebc
siv0707 <drsivasuryameenakshisundaram@gmail.com>
2026-02-25 09:11:03 +0530

    UC5:STACK BASED PLANIDROME CHECKER

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,35 @@
+import java.util.Stack;
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args){
-        System.out.println("system initialized successfully");
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push all characters into stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop and compare
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+
+        sc.close();
     }
 }
