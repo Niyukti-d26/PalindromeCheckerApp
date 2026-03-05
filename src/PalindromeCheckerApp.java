6adb5f8df74ec4f7e8bdd80696ff4c4bd5aead49
Niyukti-d26 <npjj26@gmail.com>
2026-03-05 09:59:08 +0530

    UC3 updated

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,6 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    public static boolean checkPalindrome(String text) {
-        for (int i = 0; i < text.length() / 2; i++) {
-            if (text.charAt(i) != text.charAt(text.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
     private static void reverseAndCheck(String text) {
         String reversed = "";
 
@@ -25,21 +17,11 @@ public class PalindromeCheckerApp {
         }
     }
     public static void main(String[] args) {
-        // UC1
-        System.out.println("System initialized successfully");
-        Scanner sc = new Scanner(System.in);
 
-        // UC2
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the input text: ");
         String text = sc.nextLine();
 
-        boolean isPalindrome = checkPalindrome(text);
-
-        if (isPalindrome) {
-            System.out.println("UC2: Text is a palindrome.");
-        } else {
-            System.out.println("UC2: Text is not a palindrome.");
-        }
 
         // UC3: Reverse the string and compare
         reverseAndCheck(text);
