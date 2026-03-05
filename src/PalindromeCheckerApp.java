c738ab2ede194773fa8109f016c50b8dbc81c7f2
Niyukti-d26 <npjj26@gmail.com>
2026-03-05 08:59:23 +0530

    UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        scanner.close();
+    }
+}
+class PalindromeService {
+    public boolean checkPalindrome(String input) {
+        input = input.replaceAll("\\s+", "").toLowerCase();
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
 }
